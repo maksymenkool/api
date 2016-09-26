2bd38e426314deb89d2fc56d2459fe2767fcf633
Alexey Karapetov <f3ath@users.noreply.github.com>
2016-09-25 18:34:48 -0700

    Revert "Added param minimized"

--- app/config/common.php
+++ app/config/common.php
@@ -46,7 +46,6 @@ return [
             'send-password-reset-link',
             'travel-by-category',
             'travel-by-id',
-            'travel-published-by-author',
             'travel-category',
             'travel-category-new',
             'travel-comment',
